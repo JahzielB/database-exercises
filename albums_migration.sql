5167deccd2af2d0957eef6f5aefb2faba8c903fe
JahzielB <jahzielb@gmail.com>
2020-07-07 11:26:18 -0500

    finished first exercise in basic statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    author VARCHAR(100),
+    artist VARCHAR(100),
     name VARCHAR(100),
     release_date INT,
     sales FLOAT(7,0),
