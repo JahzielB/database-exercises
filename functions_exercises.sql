e35bae2fb911ad5364d8b4aee619d9b0dd0305cc
JahzielB <jahzielb@gmail.com>
2020-07-08 19:16:40 -0500

    finished functions exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,42 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+OR (first_name = 'Vidya' AND gender = 'M')
+OR (first_name = 'Maya' AND gender = 'M');
+SELECT * FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+OR (first_name = 'Vidya' AND gender = 'M')
+OR (first_name = 'Maya' AND gender = 'M')
+ORDER BY first_name;
+SELECT * FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+   OR (first_name = 'Vidya' AND gender = 'M')
+   OR (first_name = 'Maya' AND gender = 'M')
+ORDER BY first_name ASC, last_name ASC;
+SELECT * FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+   OR (first_name = 'Vidya' AND gender = 'M')
+OR (first_name = 'Maya' AND gender = 'M')
+ORDER BY last_name ASC, first_name ASC;
+SELECT * FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+   OR (first_name = 'Vidya' AND gender = 'M')
+   OR (first_name = 'Maya' AND gender = 'M')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no ASC;
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT * FROM employees WHERE hire_date LIKE '199%';
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+SELECT DATEDIFF(CURDATE(), hire_date) FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
