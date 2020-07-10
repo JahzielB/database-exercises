32b34885783ef8b6868ea4cb1f14d521ccfabd26
JahzielB <jahzielb@gmail.com>
2020-07-10 10:25:22 -0500

    completed third part of joins exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -15,3 +15,16 @@ ON e.emp_no = dm.emp_no
 JOIN departments as d
 ON dm.dept_no = d.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+
+SELECT * FROM departments;
+
+SELECT title AS 'Title', COUNT(*) AS 'Count'
+FROM employees as e
+JOIN titles as t
+ON e.emp_no = t.emp_no
+JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_no = 'd009' AND t.to_date = '9999-01-01' GROUP BY title;
+
